6cc76e951e2c9861a15143cd658c1f07bfd92758
Thalles Carneiro <thallescarneiro@gmail.com>
2021-11-06 11:37:14 -0300

    Exercícios - JavaScript | Primeiros passos

---
+++ fundamentos/bloco_4/dia_4.1/exercises/script_exercises.js
@@ -0,0 +1,23 @@
+// Ex. 9
+const a = 1;
+const b = 3;
+const c = 5;
+
+let isOdd = false;
+
+if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0)) {
+  isOdd = true;
+};
+console.log(isOdd);
+
+// Ex. 10
+const costOfProduct = 1;
+const saleValue = 3;
+
+if (costOfProduct >= 0 && saleValue >= 0) {
+  const totalCostOfProduct = costOfProduct * 1.2;
+  const totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
